056dca994a3bcafd046b0fb3fbe3745d575aa778
Shreehari-Acharya <shreehari.acharya.06@gmail.com>
2026-01-10 20:12:22 +0530

    Init goproc project

---
+++ goproc/cmd/root.go
@@ -0,0 +1,51 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "goproc",
+	Short: "Get system process and memory information",
+	Long: `goproc is a CLI tool written in Go that provides detailed information
+about system processes and memory usage. It leverages Go's standard library
+and third-party packages to fetch and display this information in a user-friendly
+manner. Whether you're a developer, system administrator, or just curious about
+your system's performance, goproc offers a simple way to access vital system
+metrics directly from the command line.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.goproc.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
---
+++ goproc/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2026 Shreehari R Acharya shreehari.acharya.06@gmail.com
+
+*/
+package main
+
+import "github.com/Shreehari-Acharya/goproc/cmd"
+
+func main() {
+	cmd.Execute()
+}
