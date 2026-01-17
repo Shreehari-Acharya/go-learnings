63c93fd3ffbfaffe3147b3f27b0b2685a7382117
Shreehari-Acharya <shreehari.acharya.06@gmail.com>
2026-01-17 10:46:43 +0530

    remove json fields, when value is 0 or empty

--- goproc/internals/models.go
+++ goproc/internals/models.go
@@ -2,30 +2,30 @@ package internals
 
 
 type CPUInfo struct {
-    VendorId   string  `json:"vendor_id"`
-    CPUCount   int     `json:"cpu_count"`   
-    CPUFamily  string  `json:"cpu_family"`
-    ClockSpeed float64 `json:"clock_speed_mhz"`
-    ModelName  string  `json:"model_name"`
-    CoreCount  int     `json:"core_count"`   
-    CacheSize  int     `json:"cache_size_kb"`
+    VendorId   string  `json:"vendor_id,omitempty"`
+    CPUCount   int     `json:"cpu_count,omitempty"`   
+    CPUFamily  string  `json:"cpu_family,omitempty"`
+    ClockSpeed float64 `json:"clock_speed_mhz,omitempty"`
+    ModelName  string  `json:"model_name,omitempty"`
+    CoreCount  int     `json:"core_count,omitempty"`   
+    CacheSize  int     `json:"cache_size_kb,omitempty"`
 }
 
 type MemInfo struct {
-    Total     int64 `json:"total_kb"`
-    Free      int64 `json:"free_kb"`
-    Available int64 `json:"available_kb"`
-    Buffers   int64 `json:"buffers_kb"`
-    SwapTotal int64 `json:"swap_total_kb"`
-    SwapFree  int64 `json:"swap_free_kb"`
+    Total     int64 `json:"total_kb,omitempty"`
+    Free      int64 `json:"free_kb,omitempty"`
+    Available int64 `json:"available_kb,omitempty"`
+    Buffers   int64 `json:"buffers_kb,omitempty"`
+    SwapTotal int64 `json:"swap_total_kb,omitempty"`
+    SwapFree  int64 `json:"swap_free_kb,omitempty"`
 }
 
 type LoadAvg struct {
-    OneMin         float64 `json:"load_1m"`
-    FiveMin        float64 `json:"load_5m"`
-    FifteenMin     float64 `json:"load_15m"`
-    ProcessCount   int     `json:"running_processes"`
-    TotalProcesses int     `json:"total_processes"`
+    OneMin         float64 `json:"load_1m,omitempty"`
+    FiveMin        float64 `json:"load_5m,omitempty"`
+    FifteenMin     float64 `json:"load_15m,omitempty"`
+    ProcessCount   int     `json:"running_processes,omitempty"`
+    TotalProcesses int     `json:"total_processes,omitempty"`
 }
 
 type SystemInfo struct {
